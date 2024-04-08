0b147450331977c7a5b59d6a6359c605c243b957
bababoi-2 <165707934+bababoi-2@users.noreply.github.com>
2024-04-08 22:56:27 +0200

    Update crossword_helper.js

--- Crossword Helper/crossword_helper.js
+++ Crossword Helper/crossword_helper.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Helper for https://www.50plus.de/spiele/raetsel/kreuzwortraetsel-*.html
-// @author       You
+// @author       Bababoiiiii
 // @match        https://www.50plus.de/spiele/raetsel/kreuzwortraetsel-*.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=50plus.de
 // @grant        none
